80c5d3fafd1704be163dd4e51ab9814a929a466e
Andrés Nicolás Tonello <andresnicolastonello@gmail.com>
2022-08-30 21:01:21 -0300

    Added template example

---
+++ pages/examples/template/Template.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import styles from './Template.module.scss'
+import Head from 'next/head'
+import useAppContext from '../../../context/AppContext'
+
+export default function Template() {
+  const { setCurrentExample } = useAppContext()
+
+  useEffect(() => {
+    setCurrentExample({
+      title: 'Template',
+      url: 'https://github.com/andreuscafe/shitty-examples/blob/main/pages/examples/template/Template.jsx',
+    })
+  }, [setCurrentExample])
+
+  return (
+    <>
+      <Head>
+        <title>Spline Background | Shitty examples by @andreuscafe</title>
+      </Head>
+      <div className={styles.wrapper}>
+        <h1>Template</h1>
+      </div>
+    </>
+  )
+}
---
+++ pages/examples/template/index.js
@@ -0,0 +1,3 @@
+import Template from './Template.jsx'
+
+export default Template
